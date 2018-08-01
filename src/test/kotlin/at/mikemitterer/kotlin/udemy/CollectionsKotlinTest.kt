afb505cbd05ebb555f6290de5e10f25955bc320c
Mike Mitterer <office@mikemitterer.at>
2018-08-01 08:48:40 +0200

    Kotlin/KotlinTest auf aktuelle Version gezogen

--- src/test/kotlin/at/mikemitterer/kotlin/udemy/CollectionsKotlinTest.kt
+++ src/test/kotlin/at/mikemitterer/kotlin/udemy/CollectionsKotlinTest.kt
@@ -1,6 +1,7 @@
 package at.mikemitterer.kotlin.udemy
 
-import io.kotlintest.TestCaseContext
+import io.kotlintest.Description
+import io.kotlintest.TestResult
 import io.kotlintest.specs.FunSpec
 
 /**
@@ -9,28 +10,32 @@ import io.kotlintest.specs.FunSpec
  * @since   10.03.18, 19:07
  */
 class CollectionsKotlinTest : FunSpec() {
+    var started = 0L
+
     init {
-        test("Describe your test") {
+        test("Describe your test - 1") {
             println("Hallo")
             Thread.sleep(500)
         }
 
-        test("Describe your test") {
+        test("Describe your test - 2") {
             println("Hallo2")
             Thread.sleep(500)
         }
     }
 
-    override fun interceptTestCase(context: TestCaseContext, test: () -> Unit) {
-        // before
-        val started = System.currentTimeMillis()
+    override fun beforeTest(description: Description) {
+        super.beforeTest(description)
 
-        test() // don't forget to call test()!
-
-        // after
-        val finished = System.currentTimeMillis()
-        val time = finished - started
-        println("time [ms]: $time")
+        started = System.currentTimeMillis()
     }
 
+    override fun afterTest(description: Description, result: TestResult) {
+        super.afterTest(description, result)
+
+        val finished = System.currentTimeMillis()
+        val time = finished - started
+
+        println("Test '${description.name}' took time [ms]: $time\n")
+    }
 }
